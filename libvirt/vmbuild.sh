806309d53f12290548d3f7b7c9d33caf359f95d3
Manabu Ori <manabu.ori+github@gmail.com>
2015-07-21 18:02:46 +0900

    update.

--- libvirt/vmbuild.sh
+++ libvirt/vmbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
-if [ x"$#" != x"4" ]; then
-	echo "$0 name cpu memMB storageGB"
+if [ x"$#" != x"5" ]; then
+	echo "$0 name cpu memMB storageGB last_octet"
 	exit 1
 fi
 #NAME=rhel7-minimum
@@ -20,6 +20,7 @@ REPOSERVER=10.64.193.148
 CPU=$1; shift
 MEM=$1; shift
 HDD=$1; shift
+last_octet=$1; shift
 
 cat > ${NAME}-ks.cfg << EOF
 #cdrom
@@ -32,11 +33,9 @@ lang en_US.UTF-8
 
 # Network information
 #network  --bootproto=dhcp --device=eth0 --ipv6=auto
-network --device=eth0 --ipv6=auto --bootproto=dhcp
-network --onboot=yes --device=eth1 --ipv6=auto --bootproto=static --ip=10.0.1.8 --netmask=255.255.255.0 --gateway=10.0.1.8
-network --onboot=yes --device=eth2 --ipv6=auto --bootproto=static --ip=10.0.9.8 --netmask=255.255.255.0 --gateway=10.0.9.254 --nameserver=10.64.255.25
-network --onboot=yes --device=eth3 --ipv6=auto --bootproto=static --ip=10.0.0.8 --netmask=255.255.255.0 --gateway=10.0.0.8
-network --onboot=yes --device=eth4 --ipv6=auto --bootproto=static --ip=10.0.2.8 --netmask=255.255.255.0 --gateway=10.0.2.8
+network --device=eth0 --onboot=yes --ipv6=auto --bootproto=dhcp
+network --device=eth1 --onboot=yes --ipv6=auto --bootproto=static --ip=10.0.1.${last_octet} --netmask=255.255.255.0 --gateway=10.0.1.${last_octet}
+network --device=eth2 --onboot=yes --ipv6=auto --bootproto=static --ip=10.0.2.${last_octet} --netmask=255.255.255.0 --gateway=10.0.2.${last_octet}
 network  --hostname=${NAME}.osp6test.local
 # Root password
 rootpw redhat
@@ -51,50 +50,9 @@ clearpart --all --initlabel --drives=vda
 
 %packages
 @core
-#git
-#subversion
-#zsh
-#sudo
-#vim-enhanced
-#lftp
-#curl
-#wireshark
-#rsync
-#traceroute
-#wget
-#tcpdump
-#ntp
-#man
-#bind-utils
-#nfs-utils
-#autofs
-#xterm
-#sysstat
-#patch
-#strace
-#httpd
-#vsftpd
-#socat
-#nmap-ncat
-#strace
-#psmisc
-#
-#
 %end
 
 %pre
-## copied from cobbler licensed under GPLv2+
-#set -x -v
-#exec 1>/tmp/ks-pre.log 2>&1
-#
-#while : ; do
-#    sleep 10
-#    if [ -d /mnt/sysimage/root ]; then
-#        cp /tmp/ks-pre.log /mnt/sysimage/root/
-#        logger "Copied %pre section log to system"
-#        break
-#    fi
-#done &
 %end
 
 
@@ -108,58 +66,6 @@ exec 1>/root/ks-post.log 2>&1
 test -f /etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release && \
     rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release || :
 
-#sed -i.save 's/^\(%wheel.*\)ALL$/\1NOPASSWD: ALL/' /etc/sudoers
-#
-##systemctl disable NetworkManager.service
-##chkconfig network on
-#
-#cat << REPOEOF > /etc/yum.repos.d/binaries.repo
-#[binaries-rhel-7-0]
-#name=binaries: RHEL 7.0
-#baseurl=http://binaries.nrt.redhat.com/contents/RHEL/7/0/x86_64/default
-#enabled=1
-#gpgcheck=1
-#gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
-#REPOEOF
-#
-#cat << REPOEOF > /etc/yum.repos.d/localrepo-rhel7.repo
-#[local-rhel-7-server-rpms]
-#name=local-rhel-7-server-rpms
-#baseurl=http://${REPOSERVER}/repos/rhel-7-server-rpms
-#enabled=1
-#gpgcheck=0
-#
-#[local-rhel-7-server-optional-rpms]
-#name=local-rhel-7-server-optional-rpms
-#baseurl=http://${REPOSERVER}/repos/rhel-7-server-optional-rpms
-#enabled=1
-#gpgcheck=0
-#
-#[local-rhel-7-server-rh-common-rpms]
-#name=local-rhel-7-server-rh-common-rpms
-#baseurl=http://${REPOSERVER}/repos/rhel-7-server-rh-common-rpms
-#enabled=1
-#gpgcheck=0
-#
-#[local-rhel-7-server-openstack-5.0-rpms]
-#name=local-rhel-7-server-openstack-5.0-rpms
-#baseurl=http://${REPOSERVER}/repos/rhel-7-server-openstack-5.0-rpms
-#enabled=0
-#gpgcheck=0
-#
-#[local-rhel-7-server-openstack-6.0-rpms]
-#name=local-rhel-7-server-openstack-6.0-rpms
-#baseurl=http://${REPOSERVER}/repos/rhel-7-server-openstack-6.0-rpms
-#enabled=1
-#gpgcheck=0
-#
-#[local-rhel-7-server-openstack-6.0-installer-rpms]
-#name=local-rhel-7-server-openstack-6.0-installer-rpms
-#baseurl=http://${REPOSERVER}/repos/rhel-7-server-openstack-6.0-installer-rpms
-#enabled=1
-#gpgcheck=0
-#REPOEOF
-#
 %end
 
 reboot
@@ -169,36 +75,8 @@ reboot
 EOF
 
 
-#sudo setenforce 0
-#sudo mkdir -pv /var/lib/libvirt/images/${NAME}_iso
-#sudo umount -v /var/lib/libvirt/images/${NAME}_iso
-#sudo mount -vo loop ${ISO} /var/lib/libvirt/images/${NAME}_iso
-
 virsh dominfo ${NAME} && \
 { virsh destroy ${NAME}; virsh detach-disk ${NAME} --target vda --persistent && virsh undefine ${NAME}; }
-
-
-#virt-install \
-#    --connect=qemu:///system \
-#    --accelerate \
-#    --hvm \
-#    --vcpus=${CPU} \
-#    --cpu=host \
-#    --name=${NAME} \
-#    --os-type=linux \
-#    --os-variant=${VARIANT} \
-#    --ram=${MEM} \
-#    --disk path=/var/lib/libvirt/images/${NAME}.img,size=${HDD} \
-#    --disk path=${ISO},device=cdrom \
-#    --force \
-#    --network=bridge:br0,model=virtio \
-#    --network=bridge:virbr100,model=virtio \
-#    --network=bridge:virbr5,model=virtio \
-#    --location=/var/lib/libvirt/images/${NAME}_iso \
-#    --nographics \
-#    --console pty,target_type=serial \
-#    --extra-args="text console=tty0 console=ttyS0,115200 serial rd_NO_PLYMOUTH ks=file:/${NAME}-ks.cfg net.ifnames=0" \
-#    --initrd-inject=$(pwd)/${NAME}-ks.cfg
 
 virt-install \
     --connect=qemu:///system \
@@ -213,10 +91,8 @@ virt-install \
     --ram=${MEM} \
     --disk path=/var/lib/libvirt/images/${NAME}.qcow2,format=qcow2,size=${HDD},sparse=true \
     --force \
-    --network=bridge:br1,model=virtio \
+    --network=bridge:br0,model=virtio \
     --network network=mgmt,model=virtio \
-    --network network=foreman,model=virtio \
-    --network network=storage,model=virtio \
     --network network=tenant,model=virtio \
     --network network=external,model=virtio \
     --location=http://binaries.nrt.redhat.com/contents/RHEL/7/1/x86_64/default/ \
